886ffa5b006bd5b057c1f56d4d7161310c927a48
Hasan Farhan Zia <h.zia2396@gmail.com>
2026-09-13 18:43:48 -0600

    Completed Parts 1 and 2 of lab 3

---
+++ app/src/main/java/com/example/listycity3/CityRepository.kt
@@ -0,0 +1,18 @@
+package com.example.listycity3
+
+import androidx.compose.runtime.mutableStateListOf
+
+class CityRepository {
+    private val _cities = mutableStateListOf(
+        City("Edmonton", "AB"),
+        City("Vancouver", "BC"),
+        City("Toronto", "ON")
+    )
+
+    val cities: List<City>
+        get() = _cities
+
+    fun addCity(city: City) {
+        _cities.add(city)
+    }
+}
---
+++ app/src/main/java/com/example/listycity3/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.listycity3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.listycity3.CityListScreen
+import com.example.listycity3.CityRepository
+import com.example.listycity3.ui.theme.ListyCity3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val cityRepository = CityRepository()
+        setContent {
+            ListyCity3Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    CityListScreen(
+                        cities = cityRepository.cities,
+                        onAddCity = { cityRepository.addCity(it) },
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
